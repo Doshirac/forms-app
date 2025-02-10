e51b19a53b578ed1abfb907e3f1dffed9b01edf3
Doshic <kdasha09112005@gmail.com>
2025-02-10 18:53:46 +0200

    Initi i18n fopr the app

--- front/src/index.js
+++ front/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Suspense } from 'react';
 import './index.css';
 import App from './App';
+
+// import i18n (needs to be bundled ;))
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback="...is loading">
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
