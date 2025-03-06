fd1f737a4d2931dacaba085cdd0199d654c2d36c
Doshic <kdasha09112005@gmail.com>
2025-03-06 13:38:55 +0200

    Fix bug with user email in SalesForce form

--- front/src/context/AuthContext.jsx
+++ front/src/context/AuthContext.jsx
@@ -1,31 +1,43 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
   const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('jwtToken'));
   const [isAdmin, setIsAdmin] = useState(localStorage.getItem('isAdmin') === 'true');
 
   const login = (token, userData) => {
     localStorage.setItem('jwtToken', token);
     localStorage.setItem('isAdmin', userData.is_admin);
+    localStorage.setItem('user', JSON.stringify(userData));
+
     setIsAuthenticated(true);
     setIsAdmin(userData.is_admin);
+    setUser(userData);
   };
 
   const logout = () => {
     localStorage.removeItem('jwtToken');
     localStorage.removeItem('isAdmin');
+    localStorage.removeItem('user');
+
     setIsAuthenticated(false);
     setIsAdmin(false);
+    setUser(null);
   };
 
   return (
     <AuthContext.Provider value={{ 
       isAuthenticated, 
+      isAdmin,
+      user,
       login, 
-      logout,
-      isAdmin
+      logout
     }}>
       {children}
     </AuthContext.Provider>
